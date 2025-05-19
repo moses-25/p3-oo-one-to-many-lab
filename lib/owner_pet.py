e9b682128c672f78cb01ce06ba074f679e98edaf
Mosesotieno-ai <mosesotieno8363@gmail.com>
2025-05-19 22:43:18 +0300

    Implement Pet and Owner classes with basic functionality

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,28 @@
 class Pet:
-    pass
+    PET_TYPES = ["Dog", "Cat", "rodents", "Bird", "reptile", "exotic"
+    ]
+    all = []
+    def __init__(self, name, pet_type, owner = None):
+        if pet_type not in pet.PET_TYPES:
+            raise ValueError(f"Invalid pet type")
+        self.pet_type = pet_type
+        self.name = name
+        self.owner = owner
+        Pet.all.append(self)
+        pass
 
 class Owner:
+    def __init__(self, name):
+        self.name = name
+    
+    def pet(self):
+        return [pet for pet in Pet.all if pet.owner == self]
+
+    def add_pet(self, pet):
+        if not isinstance(self, pet):
+            raise Exception("pet must be an instance of Pet")
+        pet.owner = self
+
+    def get_sorted_pets(self):
+        return sorted(self.pet(), key=lambda pet: pet.name)
     pass
